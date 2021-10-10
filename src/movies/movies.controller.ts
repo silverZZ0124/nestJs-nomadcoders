e59e0f202ba2c520da654975b990b75e87a03543
silverz <liisa9601@naver.com>
2021-10-10 22:34:23 +0900

    Rest API

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,6 +1,9 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Query } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Query, Req, Res } from '@nestjs/common';
 import { Movie } from './entities/movie.entity';
 import { MoviesService } from './movies.service';
+import { CreateMovieDto } from './dto/create-movie.dto';
+import { UpdateMovieDto } from './dto/update-movie.dto';
+
 
 @Controller('movies')
 export class MoviesController {
@@ -18,26 +21,23 @@ export class MoviesController {
     // }
 
     @Get('/:id')
-    getOne(@Param("id") movieId:string):Movie{
+    getOne(@Param("id") movieId:number):Movie{
         return this.moviesService.getOne(movieId);
     }
 
     @Post()
-    create(@Body() movieData){
+    create(@Body() movieData: CreateMovieDto){
         return this.moviesService.create(movieData);
     }
 
     @Delete("/:id")
-    remove(@Param('id') movieId:string){
+    remove(@Param('id') movieId:number){
         return this.moviesService.deleteOne(movieId);
     }
 
     @Patch("/:id")
-    update(@Param('id') movieId:string, @Body() upadateData){
-        return {
-            updatedMovie: movieId,
-            ...upadateData,
-        };
+    update(@Param('id') movieId:number, @Body() upadateData:UpdateMovieDto){
+        return this.moviesService.update(movieId,upadateData);
     }
 
     
